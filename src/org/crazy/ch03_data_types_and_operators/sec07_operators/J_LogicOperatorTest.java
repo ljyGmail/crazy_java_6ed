1d9834259155e6e9f2180b3f5e43cc3cff21db2c
liangjinyong <liangjy881@gmail.com>
2024-01-09 09:12:24 +0900

    3.7.6 逻辑运算符

---
+++ src/org/crazy/ch03_data_types_and_operators/sec07_operators/J_LogicOperatorTest.java
@@ -0,0 +1,8 @@
+package org.crazy.ch03_data_types_and_operators.sec05_type_cast_for_primitives;
+
+public class J_LogicOperatorTest {
+    public static void main(String[] args) {
+        // 直接对false求非运算，将返回true
+        System.out.println(!false);
+    }
+}
