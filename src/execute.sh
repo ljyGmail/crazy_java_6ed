c575a2d92f74b72be0f5a89baca4c879e39e2304
liangjinyong <liangjy881@gmail.com>
2024-01-09 13:35:46 +0900

    4.2.1 if条件语句

--- src/execute.sh
+++ src/execute.sh
@@ -1,3 +1,12 @@
-clear &&
-javac $(find . -type f -name "*.java") -d ../target &&
-cd ../target && find . -type f -name "*.class" | sort -n | sed -e 's@^./@@' -e 's@/@.@g' -e 's@.class$@@' -e 's@^@java @' | awk '{ print "echo ======= ...", substr($2, 44), "=======;" $0, "; echo" }' | bash 2> error
+chapter=ch03
+
+if [ -n "$1" ]; then
+    chapter="$1"
+fi
+
+echo $chapter
+
+clear \
+&& javac $(find . -type f -name "*.java" | grep $chapter) -d ../target \
+&& cd ../target \
+&& find . -type f -name "*.class" | grep $chapter | sort -n | sed -e 's@^./@@' -e 's@/@.@g' -e 's@.class$@@' -e 's@^@java @' | awk '{ print "echo ======= ...", substr($2, 44), "=======;" $0, "; echo" }' | bash 2> error
---
+++ src/org/crazy/ch04_control_flow_and_arrays/sec01_branch_statements/E_IfCorrectTest2.java
@@ -0,0 +1,18 @@
+package org.crazy.ch04_control_flow_and_arrays.sec01_branch_statements;
+
+public class E_IfCorrectTest2 {
+    public static void main(String[] args) {
+        var age = 45;
+        if (age > 60) {
+            System.out.println("老年人");
+        }
+        // 在原本的if条件中增加了else的隐含条件
+        if(age > 40 && !(age > 60)) {
+            System.out.println("中年人");
+        }
+        // 在原本的if条件在增加了else的隐含条件
+        if (age > 20 && !(age > 60) && !(age > 40 && !(age > 60))) {
+            System.out.println("青年人");
+        }
+    }
+}
