e946d81034c115d4621e031a1b1416857806d518
ljyGmail <liangjy881@gmail.com>
2024-01-09 22:54:24 +0900

    4.2.2 传统switch分支语句

---
+++ src/org/crazy/ch04_control_flow_and_arrays/sec01_branch_statements/F_SwitchTest.java
@@ -0,0 +1,28 @@
+package org.crazy.ch04_control_flow_and_arrays.sec01_branch_statements;
+
+public class F_SwitchTest {
+    public static void main(String[] args) {
+        // 声明变量score，并为其赋值为'C'
+        var score = 'C';
+        // 执行switch分支语句
+        switch (score) {
+            case 'A':
+                System.out.println("优秀");
+                break;
+            case 'B':
+                System.out.println("良好");
+                break;
+            case 'C':
+                System.out.println("中");
+                break;
+            case 'D':
+                System.out.println("及格");
+                break;
+            case 'F':
+                System.out.println("不及格");
+                break;
+            default:
+                System.out.println("成绩输入错误");
+        }
+    }
+}
---
+++ src/org/crazy/ch04_control_flow_and_arrays/sec01_branch_statements/G_StringSwitchTest.java
@@ -0,0 +1,25 @@
+package org.crazy.ch04_control_flow_and_arrays.sec01_branch_statements;
+
+public class G_StringSwitchTest {
+    public static void main(String[] args) {
+        // 声明变量season
+        var season = "夏天";
+        // 执行switch分支语句
+        switch (season) {
+            case "春天":
+                System.out.println("春暖花开");
+                break;
+            case "夏天":
+                System.out.println("夏日炎炎");
+                break;
+            case "秋天":
+                System.out.println("秋高气爽");
+                break;
+            case "冬天":
+                System.out.println("冬雪皑皑");
+                break;
+            default:
+                System.out.println("季节输入错误");
+        }
+    }
+}
