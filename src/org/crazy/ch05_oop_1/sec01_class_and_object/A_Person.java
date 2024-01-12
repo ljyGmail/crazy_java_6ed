6b5e2d22b40d0e53a6ca42bf17cf87478740cea6
liangjinyong <liangjy881@gmail.com>
2024-01-12 14:30:01 +0900

    5.1.1 定义类

---
+++ src/org/crazy/ch05_oop_1/sec01_class_and_object/A_Person.java
@@ -0,0 +1,11 @@
+package org.crazy.ch05_oop_1.sec01_class_and_object;
+
+public class A_Person {
+    // 下面定义了两个成员变量
+    public String name;
+    public int age;
+    // 下面定义了一个say方法
+    public void say(String content) {
+        System.out.println(content);
+    }
+}
