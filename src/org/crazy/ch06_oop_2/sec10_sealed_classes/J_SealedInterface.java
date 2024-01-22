c2e8b47ef82794af41052a3c83cdd0449ef23680
ljyGmail <liangjy881@gmail.com>
2024-01-22 23:26:18 +0900

    6.10.3 密封接口

---
+++ src/org/crazy/ch06_oop_2/sec10_sealed_classes/J_SealedInterface.java
@@ -0,0 +1,21 @@
+package org.crazy.ch06_oop_2.sec10_sealed_classes;
+
+// 定义密封接口
+sealed interface J_Celestial {
+    void fly():
+}
+
+// 密封接口的子接口，只能用sealed或non-sealed修饰
+non-sealed interface J_Artificial extends J_Celestial {}
+
+non-sealed class J_Star implements J_Celestial {
+    public void fly() {
+        System.out.println("恒星在星系内转动");
+    }
+}
+
+final class J_Planet implements J_Celestial {
+    public void fly() {
+        System.out.println("行星绕恒星转动");
+    }
+}
