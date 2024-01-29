6ae9e52d3592648d868e870cdaf0ef56ee47f45d
ljyGmail <liangjy881@gmail.com>
2024-01-29 14:42:58 +0900

    9.1.1 在编译时不检查类型导致的异常

---
+++ src/org/crazy/ch09_generics/sec01_intro_to_generics/A_ListErrTest.java
@@ -0,0 +1,16 @@
+package org.crazy.ch09_generics.sec01_intro_to_generics;
+
+import java.util.ArrayList;
+
+public class A_ListErrTest {
+    public static void main(String[] args) {
+        // 创建一个只想保存字符串的List集合
+        var strList = new ArrayList();
+        strList.add("疯狂Java讲义");
+        strList.add("疯狂Android讲义");
+        // "不小心"把一个Integer对象"丢进"了集合中
+        strList.add(5);
+        // 下面代码引发ClassCaseException
+        // strList.forEach(str -> System.out.println(((String) str).length()));
+    }
+}
