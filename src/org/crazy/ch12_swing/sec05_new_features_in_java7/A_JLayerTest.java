7187c3b47c7a174e56f3e363c0179e6fb3a3e1be
ljyGmail <liangjy881@gmail.com>
2024-02-16 10:18:18 +0900

    12.5.1 使用JLayer装饰组件 - 毛玻璃

--- src/org/crazy/ch12_swing/sec05_new_features_in_java7/A_JLayerTest.java
+++ src/org/crazy/ch12_swing/sec05_new_features_in_java7/A_JLayerTest.java
@@ -5,6 +5,10 @@ import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -29,6 +33,52 @@ class A_FirstLayerUI extends LayerUI<JComponent> {
         // 绘制一个与被装饰组件具有相同大小的组件
         g2.fillRect(0, 0, c.getWidth(), c.getHeight());
         g2.dispose();
+    }
+}
+
+class A_BlurLayerUI extends LayerUI<JComponent> {
+    private BufferedImage screenBlurImage;
+    private BufferedImageOp operation;
+
+    public A_BlurLayerUI() {
+        var ninth = 1.0f / 9.0f;
+        // 定义模糊参数
+        float[] blurKernel = {
+            ninth, ninth, ninth,
+            ninth, ninth, ninth,
+            ninth, ninth, ninth
+        };
+
+        // ConvolveOp代表进行模糊处理，它将原图片的每一个像素的颜色与周围
+        // 像素的颜色进行混合，从而计算出当前像素的颜色值
+        operation = new ConvolveOp(
+            new Kernel(3, 3, blurKernel),
+            ConvolveOp.EDGE_NO_OP, null);
+    }
+
+    public void paint(Graphics g, JComponent c) {
+        var w = c.getWidth();
+        var h = c.getHeight();
+        // 如果被装饰窗口大小为0像素x0像素，则直接返回
+        if (w == 0 || h == 0)
+            return;
+        // 如果screenBlurImage没有初始化、或它的尺寸不对
+        if (screenBlurImage == null
+            || screenBlurImage.getWidth() != w
+            || screenBlurImage.getHeight() != h) {
+            // 重新创建新的BufferedImage
+            screenBlurImage = new BufferedImage(w,
+                    h, BufferedImage.TYPE_INT_RGB);
+        }
+
+        Graphics2D ig2 = screenBlurImage.createGraphics();
+        // 把被装饰组件的界面绘制到当前screenBlurImage上
+        ig2.setClip(g.getClip());
+        super.paint(ig2, c);
+        ig2.dispose();
+        var g2 = (Graphics2D) g;
+        // 对JLayer装饰的组件进行模糊处理
+        g2.drawImage(screenBlurImage, operation, 0, 0);
     }
 }
 
@@ -58,7 +108,8 @@ public class A_JLayerTest {
         p.add(orderButton);
 
         // 创建LayerUI对象
-        LayerUI<JComponent> layerUI = new A_FirstLayerUI();
+        // LayerUI<JComponent> layerUI = new A_FirstLayerUI();
+        LayerUI<JComponent> layerUI = new A_BlurLayerUI();
 
         // 使用layerUI来装饰指定的JPanel组件
         var layer = new JLayer<JComponent>(p, layerUI);
